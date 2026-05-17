ace530d438bd29f3d4d8c3a10a6ee8930f31f366
MehulArya <mehularya007@gmail.com>
2026-05-17 09:09:41 +0000

    Naming of socket address and binding socket address with socket

--- src/Ame_http_tcpServer.cpp
+++ src/Ame_http_tcpServer.cpp
@@ -16,7 +16,15 @@ namespace {
 
 namespace http{
 	TCPServer::TCPServer(std::string ip_address, int port) : m_ip_address(ip_address), m_port(port), m_socket(), m_new_socket(), m_incomingM	essage(), m_socketAddress(), m_scoketAddress_len(sizeof(m_socketAddress)), m_serverMessage(buildResponse()) {
-		startServer();
+		m_socketAddress.sin_family = AF_INET;     // sin_family is a member variable of IPV4 socket address structure in C and C++. Specifies what address family that the socket will use.
+		m_socketAddress.sin_port = htons(m_port); // 16 bit integer field inside IPV4 socket address structure. htons() convert bytes to TCP/IP network byte order.
+		m_socketAddress.sin_addr.s_addr = inet_addr(m_ip_address.c_str()); // Converts a IP address string into a binary format understood by OS.
+
+		if(startServer() != 0){
+			std::ostringstream ss;
+			ss << "Failed to start server with PORT: " << 	ntohs(m_socketAddress.sin_port); // ntohs is network to host short. To convert network byte order to host byte order.
+			log(ss.str());
+		};
 	}
 	TCPServer::~TCPServer(){
 		closeServer();
@@ -27,6 +35,11 @@ namespace http{
 		m_socket = socket(AF_INET, SOCK_STREAM, 0);   // Creating a socket
 		if(m_socket < 0){
 			exitWithError("Cannot Create Socket");
+			return 1;
+		}
+
+		if(bind(m_socket, (sockaddr *) &m_socketAddress, m_socketAddress_len) < 0){    // Bind Socket with the socket address
+			exitWithError("Cannot connect Socket with address");
 			return 1;
 		}
 		return 0;
